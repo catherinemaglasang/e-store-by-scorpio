54d8489a065be593c0b88e8eec78b9b0570b83dd
catherinemaglasang <maglasangcatherine12@gmail.com>
2016-02-10 20:13:27 +0800

    Edited admin dashboard

--- Code Model/estore/app/models.py
+++ Code Model/estore/app/models.py
@@ -1,14 +1,14 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects.postgresql import JSON
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy.dialects.postgresql import JSON
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
+# app = Flask(__name__)
+# db = SQLAlchemy(app)
 
-class Category(db.Model):
-    name = db.Column(db.String)
+# class Category(db.Model):
+#     name = db.Column(db.String)
 
-class Product(db.Model):
-    id = db.Column(db.String(), primary_key=True)
-    description = db.Column(db.String())
-    category_id = db.Column('Category')
+# class Product(db.Model):
+#     id = db.Column(db.String(), primary_key=True)
+#     description = db.Column(db.String())
+#     category_id = db.Column('Category')
