6d880691746cf8d3f0bb9445b55a03c1c2101a66
Roselle Ebarle <roselle.ebarle04@gmail.com>
2016-02-13 14:12:09 +0800

    Add initial api for user signup POST request

--- Code Model/estore/app/models.py
+++ Code Model/estore/app/models.py
@@ -1,35 +1,16 @@
 from app import db
+from datetime import datetime
+from flask import url_for
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    """An admin user capable of viewing reports.
+
+    :param str email: email address of user
+    :param str password: encrypted password for the user
+
+    """
+    email = db.Column(db.String, primary_key=True)
+    password = db.Column(db.String)
 
     def __repr__(self):
-        return '<User %r>' % (self.nickname)
-
-
-
-
-
-
-
-
-
-
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.dialects.postgresql import JSON
-
-# app = Flask(__name__)
-# db = SQLAlchemy(app)
-
-# class Category(db.Model):
-#     name = db.Column(db.String)
-
-# class Product(db.Model):
-#     id = db.Column(db.String(), primary_key=True)
-#     description = db.Column(db.String())
-#     category_id = db.Column('Category')
-
-
+        return '%s' % (self.email)
