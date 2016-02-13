3786a256f9f24a8127011ede93f812ff8e582176
Dexter Esin <sdexterous@yahoo.com>
2016-02-13 13:07:09 +0800

    add costumer

--- Code Model/estore/app/views.py
+++ Code Model/estore/app/views.py
@@ -8,3 +8,22 @@ from .models import *
 @app.route('/')
 def index():
 	return render_template('index.html', title='Home') 
+
+class AddCostumer(View):
+    def post(self, request):
+        # Get data
+        costumer_id = request.POST['costumer_id']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        city = request.POST['city']
+        state = request.POST['state']
+        postal_code = request.POST['postal_code']
+        country = request.POST['country']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        user_id = request.POST['user_id]
+        billing_address = request.POST['billing_address']
+        shipping_address = request.POST['shipping_address']
+        
+        
+        
