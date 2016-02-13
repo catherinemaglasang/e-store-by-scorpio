0718967b375aa6a3736076b3658f84087caf6d49
Mar Rynner Yee <marrynner.yee@g.msuiit.edu.ph>
2016-02-13 15:42:13 +0800

    [DRAFT] models.py

--- Code Model/estore/app/models.py
+++ Code Model/estore/app/models.py
@@ -9,8 +9,95 @@ class User(db.Model):
     :param str password: encrypted password for the user
 
     """
+    id = db.Column(db.Integer)
     email = db.Column(db.String, primary_key=True)
     password = db.Column(db.String)
+    is_admin = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return '%s' % (self.email)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sku = db.Column(db.String)
+    supplier_id = db.Column(db.Integer, db.ForeignKey('Supplier.id'))
+    title = db.Column(db.Integer)
+    description = db.Column(db.String)
+    category_id = db.Column(db.Integer, db.ForeignKey('Category.id'))
+    unit_price = db.Column(db.Float)
+    on_hand = db.Column(db.Integer)
+    re_order_level = db.Column(db.Integer)
+    is_active = db.Column(db.Boolean, default=True)
+
+class Supplier(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+    phone = db.Column(db.String)
+    fax = db.Column(db.String)
+    email = db.Column(db.String)
+    is_active = db.Column(db.Boolean, default=True)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.Text)
+    main_image = db.Column()
+    is_active = db.Column(db.Boolean, default=True)
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    session_id = db.Column(db.Integer)
+    date_created = db.Column(db.Integer)
+    customer_id = db.relationship()
+    is_active = db.Column(db.Boolean, default=True)
+
+class CartDetails(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cart_id = db.Column(db.Integer, db.ForeignKey('Cart.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('Product.id'))
+    quantity = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
+
+class WishlistDetails(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    wishlist_id = db.Column(db.Integer, db.ForeignKey('Wishlist.id'))
+    product_id = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime)
+
+class Wishlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    address = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String)
+    postal_code = db.Column(db.String)
+    country = db.Column(db.String)
+    phone = db.Column(db.String)
+    email = db.Column(db.String)
+    user_id = db.Column(db.Integer)
+    billing_address = db.Column(db.String)
+    shipping_address = db.Column(db.String)
+    date_created = db.Column(db.DateTime)
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('Customer.id'))
+    payment_id = db.Column(db.Integer)
+    transaction_date = db.Column(db.DateTime)
+    shipping_date = db.Column(db.DateTime)
+    timestamp = db.Column(db.DateTime)
+    transaction_status = db.Column(db.String)
+    total = db.Column(db.Float)
+
+class OrderDetails(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('Order.id'))
+    product_id = db.Column(db.Integer)
+    unit_price = db.Column(db.Float)
+    discount = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
