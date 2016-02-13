1b2739b6d8d39f5ce379bb5e008d1788852ae244
Roselle Ebarle <roselle.ebarle04@gmail.com>
2016-02-13 10:52:44 +0800

    Update code organization

--- Code Model/estore/config.py
+++ Code Model/estore/config.py
@@ -1,18 +1,18 @@
-# Statement for enabling the development environment
-DEBUG = True
-
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-CSRF_ENABLED = True
+class Config(object):
+	DEBUG = False
+	TESTING = False
+	CSRF_ENABLED = True
+	SECRET_KEY = 'this-really-needs-to-be-changed'
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
+class ProductionConfig(Config):
+    DEBUG = False
 
-# Use a secure, unique and absolutely secret key for
-# signing the data. 
-CSRF_SESSION_KEY = "secret"
-
-# Secret key for signing cookies
-SECRET_KEY = "secret"
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    TESTING = True
+    
+print(os.environ['DATABASE_URL'])
