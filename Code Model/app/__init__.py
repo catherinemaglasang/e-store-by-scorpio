7cd79d50e0633e6f7a9c82630e6bafcb57748edc
catherinemaglasang <maglasangcatherine12@gmail.com>
2016-02-20 09:49:01 +0800

    Deleted unused files

--- Code Model/app/__init__.py
+++ Code Model/app/__init__.py
@@ -2,25 +2,9 @@ import os
 import flask
 import sys
 from flask import Flask, jsonify
-from app.models import DBconn
+
 
 app = Flask(__name__)
-<<<<<<< HEAD:Code Model/app/__init__.py
 
-=======
->>>>>>> a74407d51d360d03b2835b3ac16b974d32bf91fc:Code Model/estore/app/__init__.py
-
-def spcall(qry, param, commit=False):
-    try:
-        dbo = DBconn()
-        cursor = dbo.getcursor()
-        cursor.callproc(qry, param)
-        res = cursor.fetchall()
-        if commit:
-            dbo.dbcommit()
-        return res
-    except:
-        res = [("Error: " + str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]),)]
-    return res
 
 from app import views
