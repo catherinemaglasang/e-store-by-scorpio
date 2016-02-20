f04daa31ebc671c8329bb0c14beab113a92de096
catherinemaglasang <maglasangcatherine12@gmail.com>
2016-02-20 10:45:36 +0800

    Created table products

--- Code Model/app/postgres.sql
+++ Code Model/app/postgres.sql
@@ -1,20 +1,26 @@
-create table tasks (
+create table products (
    id int8 primary key,
+   sku text,
+   supplier_id int8,
    title text,
    description text,
-   done boolean
+   category_id int8,
+   unit_price float8,
+   on_hand int8,
+   re_order_level int8,
+   is_active boolean
 );
 
-create or replace function newtask(par_id int8, par_title  text, par_description text, par_done boolean) returns text as
+create or replace function new_product(par_id int8, par_sku text, par_supplier_id int8, par_title  text, par_description text, par_category_id int8, par_unit_price float8, par_on_hand int8, par_re_order_level int8, par_is_active boolean) returns text as
 $$
   declare
     loc_id text;
     loc_res text;
   begin
-     select into loc_id id from tasks where id = par_id;
+     select into loc_id id from products where id = par_id;
      if loc_id isnull then
 
-       insert into tasks (id, title, description, done) values (par_id, par_title, par_description, par_done);
+       insert into products(id, sku, supplier_id, title, description, category_id, unit_price, on_hand, re_order_level, is_active) values (par_id, par_sku, par_supplier_id, par_title, par_description, par_category_id, par_unit_price, par_on_hand, par_re_order_level, par_is_active);
        loc_res = 'OK';
 
      else
@@ -25,24 +31,24 @@ $$
 $$
  language 'plpgsql';
 
---select newtask(1, 'Buy Groceries','Milk, Cheese, Pizza, Fruit, Tylenol', false); 
---select newtask(2, 'Learn Python','Need to find a good Python tutorial on the web', false); 
+--select new_product(1, '0111AB',, 11, 'Webster', 'Dictionary', '0111', 999.99, 20, 10, false); 
+--select new_product(2, '0222AB', 22, 'Python', 'Learning Python', '0222', 1500.00, 20, 10, false); 
 
-create or replace function gettasks(out int8, out text, out text, out boolean) returns setof record as
+create or replace function get_product(out int8, out text, out int8, out text, out text, out int8, out float8, out int8, out int8, out boolean) returns setof record as
 $$
-   select id, title, description, done from tasks;
-
-$$
- language 'sql';
- 
---select * from gettasks();
-
-create or replace function gettaskid(in par_id int8, out text, out text, out boolean) returns setof record as
-$$
-   select title, description, done from tasks where id = par_id;
+   select id, sku, supplier_id, title, description, category_id, unit_price, on_hand, re_order_level, is_active from products;
 
 $$
  language 'sql';
  
---select * from gettaskid(2);
+--select * from get_products();
+
+create or replace function get_product_id(in par_id int8, out text, out int8, out text, out text, out int8, out float8, out int8, out int8, out boolean) returns setof record as
+$$
+   select sku, supplier_id, title, description, category_id, unit_price, on_hand, re_order_level, is_active from products where id = par_id;
+
+$$
+ language 'sql';
+ 
+--select * from get_product_id(2);
 
