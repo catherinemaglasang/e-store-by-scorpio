ba7ddbaf2593def0ce58fbee91fd7f17916874f9
Roselle Ebarle <roselle.ebarle04@gmail.com>
2016-02-28 04:08:28 +0800

    Add admin lte admin dashboard theme

---
+++ Code Model/frontend/static/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#header").load("/static/partials/header.html");
+    $("#footer").load("/static/partials/footer.html");
+    $("#sidebar").load("/static/partials/sidebar.html");
+    $("#content").load("/static/partials/home.html");
+})
