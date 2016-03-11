4934d73fae6ecac3a1a1648c118d598f9a3a0fa1
Roselle Ebarle <roselle.ebarle04@gmail.com>
2016-03-11 17:12:59 +0800

    Complete feature test for product

--- Code Model/test/features/product.py
+++ Code Model/test/features/product.py
@@ -23,12 +23,13 @@ def when_i_retrieve_the_product1(step, id):
     """
     :type step: lettuce.core.Step
     """
-    world.response = world.app.get('/api/v1/product/{}'.format(id))
+    world.response = world.app.get('/api/v1/products/{}/'.format(id))
 
 
 @step("I should get a \'(.*)\' response")
-def then_i_should_get_a_202_response(step, expected_status_code):
+def then_i_should_get_a_200_response(step, expected_status_code):
     """
+    :param expected_status_code:
     :type step: lettuce.core.Step
     """
     assert_equals(world.response.status_code, int(expected_status_code))
