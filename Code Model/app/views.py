35e4f837b6f1d00e9e2781cd0a995942e5c95b0f
catherinemaglasang <maglasangcatherine12@gmail.com>
2016-03-11 17:23:49 +0800

    Updated suppliers.py

--- Code Model/app/views.py
+++ Code Model/app/views.py
@@ -7,6 +7,7 @@ from app import app
 from .models import DBconn
 
 PRODUCTS = {}
+SUPPLIERS = {}
 
 def spcall(qry, param, commit=False):
     try:
--- Code Model/test/features/supplier.py
+++ Code Model/test/features/supplier.py
@@ -1,25 +1,44 @@
-from lettuce import step, world
+import json
+
+from lettuce import step, world, before
+from nose.tools import assert_equals
+
+from app import app
+from app.views import SUPPLIERS
+
+
+@before.all
+def before_all():
+    world.app = app.test_client()
 
 
 @step("some suppliers are in a system")
-def step_impl(step):
+def given_some_suppliers_are_in_the_system(step):
     """
     :type step: lettuce.core.Step
     """
-    pass
+    SUPPLIERS.update({'1': {'id': '1', 'name': 'Bruce', 'address': 'Tibanga', 'phone': '222-2222', 'fax': '100', 'email': 'bruce@gmail.com', 'is_active': 'true'}})
 
 
-@step("I retrieve the supplier '1'")
-def step_impl(step):
+@step("I retrieve the supplier \'(.*)\'")
+def when_I_retrieve_the_supplier1(step, id):
     """
     :type step: lettuce.core.Step
     """
-    pass
+    world.response = world.app.get('/api/v1/suppliers/{}/'.format(id))
+
+
+@step("I should get a \'(.*)\' response")
+def then_i_should_get_a_202_response(step, expected_status_code):
+    """
+    :type step: lettuce.core.Step
+    """
+    assert_equals(world.response.status_code, int(expected_status_code))
 
 
 @step("the following supplier details are returned:")
-def step_impl(step):
+def the_following_supplier_details(step):
     """
     :type step: lettuce.core.Step
     """
-    pass
+    assert_equals(step.hashes, [json.loads(world.response.data)])
