498a25fb627399082d2866cfe98d616eab8a6670
Dexter Esin <sdexterous@yahoo.com>
2016-03-13 17:46:39 +0800

    categories

---
+++ Code Model/tests/features/category.py
@@ -0,0 +1,44 @@
+import json
+
+from lettuce import step, world, before
+from nose.tools import assert_equals
+
+from app import app
+from app.views import CATEGORIES
+
+
+@before.all
+def before_all():
+    world.app = app.test_client()
+
+
+@step("some categories are in a system")
+def given_some_categories_are_in_the_system(step):
+    """
+    :type step: lettuce.core.Step
+    """
+    CATEGORIES.update({'id': '3', 'name': 'pro gear', 'description': 'signature team gear', 'main_image': 'team gear.jpg', 'parent_category_id': '1', 'is_actice': 'True'})
+
+
+@step("I retrieve the category \'(.*)\'")
+def when_I_retrieve_the_category1(step, id):
+    """
+    :type step: lettuce.core.Step
+    """
+    world.response = world.app.get('/api/v1/product_category/{}/'.format(id))
+
+
+@step("I should get a \'(.*)\' response")
+def then_i_should_get_a_200_response(step, expected_status_code):
+    """
+    :type step: lettuce.core.Step
+    """
+    assert_equals(world.response.status_code, int(expected_status_code))
+
+
+@step("the following category details are returned:")
+def the_following_category_details(step):
+    """
+    :type step: lettuce.core.Step
+    """
+    assert_equals(step.hashes, [json.loads(world.response.data)])
