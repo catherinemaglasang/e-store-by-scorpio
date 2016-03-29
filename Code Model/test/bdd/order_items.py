4c47345393cc0ad438504d94903ffa512be9cdfb
Roselle Ebarle <roselle.ebarle04@gmail.com>
2016-03-29 17:07:44 +0800

    Fix error

--- Code Model/test/bdd/order_items.py
+++ Code Model/test/bdd/order_items.py
@@ -89,7 +89,6 @@ def given_order_item_id1_is_in_the_system(step, id):
     :param id:
     :type step: lettuce.core.Step
     """
-    world.browser = TestApp(app)
     world.order = world.app.get('/api/v1/order_items/{}/'.format(id))
     world.resp = json.loads(world.order.data)
     assert_equals(world.resp['status'], 'ok')
