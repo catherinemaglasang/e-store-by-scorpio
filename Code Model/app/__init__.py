3a0f0c2fed1a8e08dd7b11817c2489f05e73dd9f
Roselle Ebarle <roselle.ebarle04@gmail.com>
2016-04-06 20:22:38 +0800

    Add JSON encoder extension for serializing decimal instances

--- Code Model/app/__init__.py
+++ Code Model/app/__init__.py
@@ -1,3 +1,5 @@
+import decimal
+import flask.json
 from flask import Flask, Blueprint, current_app
 from config import config
 from flask.ext.cors import CORS
@@ -5,10 +7,21 @@ from flask.ext.cors import CORS
 api = Blueprint('api', __name__)
 
 
+class MyJSONEncoder(flask.json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            # Convert decimal instances to strings.
+            return str(obj)
+        return super(MyJSONEncoder, self).default(obj)
+
+
 def create_app(_config='development'):
     app = Flask(__name__)
+
     CORS(app)
+
     app.config.from_object(config[_config])
+    app.json_encoder = MyJSONEncoder
 
     from app.inventory import controller
     from app import views
