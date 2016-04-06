e787f82c4d27e7a03a5efcd903fac66d411b1618
Roselle Ebarle <roselle.ebarle04@gmail.com>
2016-04-06 22:47:41 +0800

    Modularize itemDetail controller

--- Code Model/frontend/static/app/controllers/main.controller.js
+++ Code Model/frontend/static/app/controllers/main.controller.js
@@ -62,25 +62,7 @@ mainApp.controller('MainController', ['$scope', '$http', '$location', 'Item', 'L
     $scope.initialize();
 }]);
 
-mainApp.controller('ItemDetailController', ['$scope', '$http', '$location', 'Item', 'Location', 'Attribute', '$routeParams', function ($scope, $http, $location, Item, Location, Attribute, $routeParams) {
-    var itemId = $routeParams.id;
 
-    $scope.item = '';
-
-    $scope.getItemDetail = function () {
-        Item.get({id: itemId}, function (data) {
-            $scope.item = data.entries[0];
-        });
-    };
-
-    $scope.updateItemDetail = function () {
-        Item.update({id: itemId}, $scope.item, function () {
-            $location.path('/dashboard/items/all');
-        });
-    };
-
-    $scope.getItemDetail();
-}]);
 
 mainApp.controller('LocationDetailController', ['$scope', '$http', '$location', 'Item', 'Location', 'Attribute', '$routeParams', function ($scope, $http, $location, Item, Location, Attribute, $routeParams) {
     var locationId = $routeParams.id;
