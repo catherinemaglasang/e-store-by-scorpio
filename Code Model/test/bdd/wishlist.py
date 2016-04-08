2d9393a009d8d9b3510ca15b3f86139bb69a5f0a
Dexter Esin <sdexterous@yahoo.com>
2016-04-08 09:19:06 +0800

    add steps for create new wishlist

--- Code Model/test/bdd/wishlist.py
+++ Code Model/test/bdd/wishlist.py
@@ -21,10 +21,10 @@ def given_wishlist_is_in_the_system(step, id):
     :param id:
     :type step: lettuce.core.Step
     """
-
-    world.resp = world.app.get('/api/v1/wishlist/'.format(id))
+    # raise Exception(world.app.get('/api/v1/wishlist/'.format(id)))
+    world.resp = world.browser.get('/api/v1/wishlist/'.format(id))
     data = json.loads(world.resp.data)
-    #raise Exception(json.loads(world.resp.data))
+    # raise Exception(json.loads(world.resp.data))
     assert_equals(data['status'], 'ok')
 
 
@@ -34,7 +34,7 @@ def when_I_retrieve_the_wishlist(step, id):
     :param id:
     :type step: lettuce.core.Step
     """
-    world.response = world.app.get('/api/v1/wishlist/'.format(id))
+    world.response = world.browser.get('/api/v1/wishlist/'.format(id))
 
 
 @step("I should have a status code response \'(.*)\'")
@@ -69,13 +69,13 @@ def given_I_retrieve_a_wishlist_with_resource_url(step, id):
     world.wishlist_uri = '/api/v1/wishlist/%s/' % (id)
 
 
-@step("I retrieve the JSON result")
-def when_I_retrieve_the_JSON_result(step):
+@step("I retrieve the wishlist JSON result")
+def when_I_retrieve_the_wishlist_JSON_result(step):
     """
     :type step: lettuce.core.Step
     """
     #raise Exception(world.wishlist_uri)
-    world.response = world.app.get(world.wishlist_uri)
+    world.response = world.browser.get(world.wishlist_uri)
 
 
 @step("I should have a status code response \'(.*)\'")
@@ -121,3 +121,55 @@ def and_it_should_have_an_empty_field_entries(step):
     :type step: lettuce.core.Step
     """
     assert_equals(len(world.resp['entries']), 0)
+
+
+
+""" Create Wishlist """
+
+
+
+@step('I have the details of wishlist')
+def step_impl(step):
+    """
+    :type step: lettuce.core.Step
+    """
+    world.wishlist1 = step.hashes[0]
+
+
+@step("I POST to url \'(.*)\' the wishlist")
+def step_impl(step, url):
+    """
+    :type step: lettuce.core.Step
+    """
+    world.wishlist_post_uri = url
+    world.wishlist_post_response = world.browser.post(world.wishlist_post_uri, data=json.dumps(world.wishlist1))
+
+
+@step("I should get \'(.*)\' status code response")
+def step_impl(step, expected_status_code):
+    """
+    :param expected_status_code:
+    :type step: lettuce.core.Step
+    """
+    # raise Exception(world.wishlist_post_response.status_code, int(expected_status_code))
+    assert_equals(world.wishlist_post_response.status_code, int(expected_status_code))
+
+
+@step("I should get \'(.*)\' status")
+def step_impl(step, status):
+    """
+    :type step: lettuce.core.Step
+    """
+    # raise Exception(type(world.wishlist_post_response.data))
+    world.wishlist_post_response_json = json.loads(world.wishlist_post_response.data)
+    assert_equals(world.wishlist_post_response_json['status'], status)
+
+
+@step("I should get \'(.*)\' message")
+def step_impl(step, message):
+    """
+    :type step: lettuce.core.Step
+    """
+    assert_equals(world.wishlist_post_response_json['message'], message)
+
+    
